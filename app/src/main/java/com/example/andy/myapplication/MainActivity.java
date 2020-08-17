1f7e6e8f715d918e782416f65092b09948a826f4
mizanursys <mizanursys94@gmail.com>
2020-08-17 14:16:26 +0600

    asdkjhk

--- app/src/main/java/com/example/andy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/andy/myapplication/MainActivity.java
@@ -16,12 +16,16 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<SubjectData> arrayList = new ArrayList<SubjectData>();
 
 
-        arrayList.add(new SubjectData("Lite App One", "https://www.tutorialspoint.com/java/", "Start 1231"));
-        arrayList.add(new SubjectData("Lite App Two", "https://www.tutorialspoint.com/python/", "Start 1232"));
-        arrayList.add(new SubjectData("Lite App Three", "https://www.tutorialspoint.com/javascript/", "Start 1233"));
-        arrayList.add(new SubjectData("Lite App Four", "https://www.tutorialspoint.com/cprogramming/", "Start 1234"));
-        arrayList.add(new SubjectData("Lite App Five", "https://www.tutorialspoint.com/cplusplus/", "Start 1235"));
-        arrayList.add(new SubjectData("Lite App Six", "https://www.tutorialspoint.com/android/", "Start 1236"));
+        arrayList.add(new SubjectData("Lite App One", "APP_345345", "Start 1231"));
+        arrayList.add(new SubjectData("Lite App Two", "APP_345345", "Start 1232"));
+        arrayList.add(new SubjectData("Lite App Three", "APP_345345", "Start 1233"));
+        arrayList.add(new SubjectData("Lite App Four", "APP_345345", "Start 1234"));
+        arrayList.add(new SubjectData("Lite App Five", "APP_345345", "Start 1235"));
+        arrayList.add(new SubjectData("Lite App Six", "APP_345345", "Start 1236"));
+        arrayList.add(new SubjectData("Lite App Six", "APP_345345", "Start 1236"));
+        arrayList.add(new SubjectData("Lite App Six", "APP_345345", "Start 1236"));
+        arrayList.add(new SubjectData("Lite App Six", "APP_345345", "Start 1236"));
+        arrayList.add(new SubjectData("Lite App Six", "APP_345345", "Start 1236"));
 
         CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
         list.setAdapter(customAdapter);
